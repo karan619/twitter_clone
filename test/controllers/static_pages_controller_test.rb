aa1cf07ed695f87af906509f7d3dc39039f8156a
karan619 <karan.valecha92@gmail.com>
2015-07-03 23:54:10 +0530

    Added guard for automaard for automated testing

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @BaseTitle = "Twitter Clone"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Twitter Clone"
+    assert_select "title", "Home | #{@BaseTitle}" 
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Twitter Clone"
+    assert_select "title", "Help | #{@BaseTitle}" 
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Twitter Clone"
+    assert_select "title", "About | #{@BaseTitle}"
   end
 end
