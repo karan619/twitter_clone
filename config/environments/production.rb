ce70ed63d9cb6b231853de2b2156de93e91d63c4
karan619 <karan.valecha92@gmail.com>
2015-07-16 00:31:07 +0530

    final touch

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   # Configuration for sendgrid on heroku
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'twitter619.herokuapp.com'
+  host = 'tweetposts.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
